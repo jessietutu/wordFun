7ef685cc3ed74eb6b5a1d4a6157267c54c88abfc
jessietutu <yangbaifan@hotmail.com>
2018-10-08 17:19:45 -0700

    Initial Commit

---
+++ wordFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wordFun
+//
+//  Created by Jessie on 2018/10/8.
+//  Copyright © 2018年 Jessie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
